feec3cc15ef67d54e645f7ba627590bde580137e
Adam Booth <adam_booth@live.co.uk>
2025-11-27 15:46:13 +0000

    restarted a different way

--- app.js
+++ app.js
@@ -8,50 +8,15 @@ const imgIndexArray = [
   "tiger.jpg",
 ];
 
-const photoGallery = document.getElementById("photo-gallery");
-
-let currentImgIndex = 0;
-
-photoGallery.src = `./media/images/${imgIndexArray[currentImgIndex]}`;
-
-const nextImageBtn = document.getElementById("next-img-btn");
-
-const previousImageBtn = document.getElementById("previous-img-btn");
-
-nextImageBtn.addEventListener("click", function () {
-  if (currentImgIndex === 0) {
-    currentImgIndex = 1;
-    photoGallery.src = `./media/images/${imgIndexArray[currentImgIndex]}`;
-  } else if (currentImgIndex === 1) {
-    currentImgIndex = 2;
-    photoGallery.src = `./media/images/${imgIndexArray[currentImgIndex]}`;
-  } else if (currentImgIndex === 2) {
-    currentImgIndex = 3;
-    photoGallery.src = `./media/images/${imgIndexArray[currentImgIndex]}`;
-  } else if (currentImgIndex === 3) {
-    currentImgIndex = 4;
-    photoGallery.src = `./media/images/${imgIndexArray[currentImgIndex]}`;
-  } else if (currentImgIndex === 4) {
-    currentImgIndex = 0;
-    photoGallery.src = `./media/images/${imgIndexArray[currentImgIndex]}`;
-  }
-});
-
-previousImageBtn.addEventListener("click", function () {
-  if (currentImgIndex === 4) {
-    currentImgIndex = 3;
-    photoGallery.src = `./media/images/${imgIndexArray[currentImgIndex]}`;
-  } else if (currentImgIndex === 3) {
-    currentImgIndex = 2;
-    photoGallery.src = `./media/images/${imgIndexArray[currentImgIndex]}`;
-  } else if (currentImgIndex === 2) {
-    currentImgIndex = 1;
-    photoGallery.src = `./media/images/${imgIndexArray[currentImgIndex]}`;
-  } else if (currentImgIndex === 1) {
-    currentImgIndex = 0;
-    photoGallery.src = `./media/images/${imgIndexArray[currentImgIndex]}`;
-  } else if (currentImgIndex === 0) {
-    currentImgIndex = 4;
-    photoGallery.src = `./media/images/${imgIndexArray[currentImgIndex]}`;
-  }
-});
+const imageData = [
+  {
+    imageName: "",
+    imageSrc: "",
+    imageAlt: "",
+  },
+  {
+    imageName: "",
+    imageSrc: "",
+    imageAlt: "",
+  },
+];
