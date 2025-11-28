2ff3b475eb4b4ec49397190a76d1edfbe0066327
Adam Booth <adam_booth@live.co.uk>
2025-11-28 11:38:07 +0000

    all working need to fix responsiveness

--- app.js
+++ app.js
@@ -8,43 +8,54 @@ const imagesArray = [
     imageSrc: "./media/images/elephant.jpg",
     imageAlt: "picture of elephants",
     imgIndex: 0,
+    tabIndex: 2,
   },
   {
     idName: "foxImg",
     imageSrc: "./media/images/fox.jpg",
     imageAlt: "picture of a fox",
     imgIndex: 1,
+    tabIndex: 3,
   },
   {
     idName: "leopardImg",
     imageSrc: "./media/images/leopard.jpg",
     imageAlt: "picture of a leopard",
     imgIndex: 2,
+    tabIndex: 4,
   },
   {
     idName: "raccoonImg",
     imageSrc: "./media/images/raccoon.jpg",
     imageAlt: "picture of a raccoon",
     imgIndex: 3,
+    tabIndex: 5,
   },
   {
     idName: "tigerImg",
     imageSrc: "./media/images/tiger.jpg",
     imageAlt: "picture of a tiger",
     imgIndex: 4,
+    tabIndex: 6,
   },
 ];
 
 const fullscreenContainer = document.getElementById("fullscreen-container");
+const nextBtnContainer = document.getElementById("next-btn-container");
+const previousBtnContainer = document.getElementById("previous-btn-container");
 const nextBtn = document.createElement("button");
 const previousBtn = document.createElement("button");
 const thumbnailContainer = document.getElementById("thumbnail-container");
 nextBtn.id = "next-btn";
 nextBtn.textContent = ">";
+nextBtn.tabIndex = 7;
 previousBtn.id = "previous-btn";
 previousBtn.textContent = "<";
+previousBtn.tabIndex = 1;
 nextBtn.setAttribute("aria-label", "Next image");
 previousBtn.setAttribute("aria-label", "Previous image");
+previousBtnContainer.appendChild(previousBtn);
+nextBtnContainer.appendChild(nextBtn);
 
 let currentImageIndex = 0;
 
@@ -54,9 +65,7 @@ function displayFullscreenImage(index) {
   fullscreenImg.id = "fullscreen-img";
   fullscreenImg.src = imagesArray[index].imageSrc;
   fullscreenImg.alt = imagesArray[index].imageAlt;
-  fullscreenContainer.appendChild(previousBtn);
   fullscreenContainer.appendChild(fullscreenImg);
-  fullscreenContainer.appendChild(nextBtn);
 }
 
 nextBtn.addEventListener("click", function () {
@@ -85,8 +94,9 @@ function createThumbnails() {
     img.id = "thumbnail-img";
     img.src = imagesArray[i].imageSrc;
     img.alt = imagesArray[i].imageAlt;
-
+    img.tabIndex = imagesArray[i].tabIndex;
     let index = imagesArray[i].imgIndex;
+    img.imgIndex = index;
 
     img.addEventListener("click", () => {
       displayFullscreenImage(index);
@@ -101,6 +111,13 @@ document.addEventListener("keydown", function (event) {
     nextBtn.click();
   } else if (event.key === "ArrowLeft") {
     previousBtn.click();
+  }
+  if (event.key === "Enter") {
+    const focusedElement = document.activeElement;
+
+    if (focusedElement.imgIndex !== undefined) {
+      displayFullscreenImage(focusedElement.imgIndex);
+    }
   }
 });
 
